3c6c970de32335991f74f4c6c995a027e2f987ec
Dan Tomosoiu <dan.tomosoiu@gmail.com>
2012-12-05 12:42:46 +0000

    Visualising the mesh works (optimising doesn't, yet).

--- Model/JMonkey Project/Michael/src/mygame/Main.java
+++ Model/JMonkey Project/Michael/src/mygame/Main.java
@@ -1,5 +1,6 @@
 package mygame;
 
+import com.bulletphysics.collision.shapes.TriangleShape;
 import com.jme3.app.SimpleApplication;
 import com.jme3.input.KeyInput;
 import com.jme3.material.Material;
@@ -13,17 +14,20 @@ import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.scene.VertexBuffer;
 import jme3tools.navmesh.NavMesh;
+import jme3tools.navmesh.util.NavMeshGenerator;
 
 /**
  * test
+ *
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
 
     boolean wireframe;
     Material mat;
-    
+
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
@@ -31,26 +35,73 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        
+
         inputManager.addMapping("toggle wireframe", new KeyTrigger(KeyInput.KEY_T));
         inputManager.addListener(actionListener, "toggle wireframe");
-        
+
         Spatial ship = assetManager.loadModel("Models/AccurateModelExp/AccurateModelExp.j3o");
-        
-        
+
+
         Node node;
         node = (Node) ship;
         Node chil1 = (Node) node.getChildren().get(2);
         Geometry chil = (Geometry) chil1.getChildren().get(0);
+
+        
         
         Mesh shipMesh = chil.getMesh();
         NavMesh shipNM = new NavMesh();
+        
+        //NavMeshGenerator generator = new NavMeshGenerator();
+        
+        //Mesh optimisedMesh = generator.optimize(shipMesh);
+        
         shipNM.loadFromMesh(shipMesh);
-        
-        
-        System.out.println(chil1.getChildren().size());
-        mat = chil.getMaterial();
-        rootNode.attachChild(chil1);
+
+
+        for (int i = 0; i < shipNM.getNumCells(); i++) {
+            TriangleShape tr;
+            Vector3f v0 = shipNM.getCell(i).getVertex(0);
+            Vector3f v1 = shipNM.getCell(i).getVertex(1);
+            Vector3f v2 = shipNM.getCell(i).getVertex(2);
+
+            Mesh lineMesh = new Mesh();
+            lineMesh.setMode(Mesh.Mode.Lines);
+            lineMesh.setBuffer(VertexBuffer.Type.Position, 3, new float[]{v0.x, v0.y, v0.z, v1.x, v1.y, v1.z});
+            lineMesh.setBuffer(VertexBuffer.Type.Index, 2, new short[]{0, 1});
+            lineMesh.updateBound();
+            lineMesh.updateCounts();
+            Geometry lineGeometry = new Geometry("line", lineMesh);
+            Material lineMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            lineGeometry.setMaterial(lineMaterial);
+            rootNode.attachChild(lineGeometry);
+            
+            lineMesh = new Mesh();
+            lineMesh.setMode(Mesh.Mode.Lines);
+            lineMesh.setBuffer(VertexBuffer.Type.Position, 3, new float[]{v0.x, v0.y, v0.z, v2.x, v2.y, v2.z});
+            lineMesh.setBuffer(VertexBuffer.Type.Index, 2, new short[]{0, 1});
+            lineMesh.updateBound();
+            lineMesh.updateCounts();
+            lineGeometry = new Geometry("line", lineMesh);
+            lineMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            lineGeometry.setMaterial(lineMaterial);
+            rootNode.attachChild(lineGeometry);
+            
+            lineMesh = new Mesh();
+            lineMesh.setMode(Mesh.Mode.Lines);
+            lineMesh.setBuffer(VertexBuffer.Type.Position, 3, new float[]{v2.x, v2.y, v2.z, v1.x, v1.y, v1.z});
+            lineMesh.setBuffer(VertexBuffer.Type.Index, 2, new short[]{0, 1});
+            lineMesh.updateBound();
+            lineMesh.updateCounts();
+            lineGeometry = new Geometry("line", lineMesh);
+            lineMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            lineGeometry.setMaterial(lineMaterial);
+            rootNode.attachChild(lineGeometry);
+        }
+
+        //System.out.println(chil1.getChildren().size());
+        //mat = chil.getMaterial();
+        //rootNode.attachChild(chil1);
     }
 
     @Override
@@ -62,17 +113,15 @@ public class Main extends SimpleApplication {
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
     }
-    
     private ActionListener actionListener = new ActionListener() {
-    @Override
-    public void onAction(String name, boolean pressed, float tpf) {
-      // toggle wireframe
-      if (name.equals("toggle wireframe") && !pressed) {
-        wireframe = !wireframe; // toggle boolean
-        mat.getAdditionalRenderState().setWireframe(wireframe); 
-      }
-      // else ... other input tests.
-    }
-  };
-    
+        @Override
+        public void onAction(String name, boolean pressed, float tpf) {
+            // toggle wireframe
+            if (name.equals("toggle wireframe") && !pressed) {
+                wireframe = !wireframe; // toggle boolean
+                mat.getAdditionalRenderState().setWireframe(wireframe);
+            }
+            // else ... other input tests.
+        }
+    };
 }
