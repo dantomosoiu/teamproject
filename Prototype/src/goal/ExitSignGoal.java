caa03d8323f757227a02a01e873fb34d191bca82
Tony Lau <1102266l@student.gla.ac.uk>
2013-02-01 09:57:01 +0000

    Goal parser and exit sign goal updated

--- Prototype/src/goal/ExitSignGoal.java
+++ Prototype/src/goal/ExitSignGoal.java
@@ -4,10 +4,31 @@
  */
 package goal;
 
+import population.Person;
+
+import com.jme3.math.Vector3f;
+
 /**
  *
- * @author 1003819k
+ * @author 1003819k/1102266l
  */
-public class ExitSignGoal {
-    
+public class ExitSignGoal extends Goal {
+
+	private  Vector3f sideA;
+	private  Vector3f sideB;
+	private  Vector3f normal;
+
+	public ExitSignGoal(Vector3f location,Vector3f n ,float c) {
+		super(location);
+		normal = n.normalize();
+		sideA = location.add(normal.mult(c));
+		sideB = location.subtract(normal.mult(c));
+
+	}
+
+
+	public Vector3f registerWithGoal(Person p){
+		return null;
+	}
+
 }
